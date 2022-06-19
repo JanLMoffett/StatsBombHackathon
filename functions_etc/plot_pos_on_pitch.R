8e6391eb7fcbc357ca53b924df9dfc07d9552a1f
Jan Moffett <49538827+JanLMoffett@users.noreply.github.com>
2022-06-19 11:56:17 -0400

    generic version

---
+++ functions_etc/plot_pos_on_pitch.R
@@ -0,0 +1,25 @@
+
+library(tidyverse)
+
+source("functions_etc/bombViz.R")
+posAb <- read.csv("functions_etc/positionDisplay.csv")
+
+
+plot_pos_on_pitch <- function(){
+  
+  plot_pitch(lineColor = shUEFA["blueLt"]) + shUEFA_theme + 
+    theme(axis.title = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          axis.line = element_blank()) + 
+    
+    labs(title = "") + 
+    
+    annotate("text",
+             x = posAb$display_x,
+             y = posAb$display_y,
+             label = paste0(posAb$position.id, "   ", posAb$position_abbr), 
+             color = shUEFA["orange"])
+  
+  
+}
